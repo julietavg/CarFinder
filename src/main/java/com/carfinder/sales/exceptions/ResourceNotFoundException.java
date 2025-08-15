547bdb9b1519112461555ccb2ab8b2b8204c5328
TheE4g1eClaw <Enrique.Hernandez2@solera.com>
2025-08-15 12:28:08 -0600

    fix: clean main folder from main branch

--- src/main/java/com/carfinder/sales/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.carfinder.sales.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
